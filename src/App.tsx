2d73434ccf1a52a3a4f0302a4cda06b9208a3da4
Administrateur <Administrateur@MacBook-Pro.lan>
2023-02-04 14:35:11 +0100

    version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface Beer {
+  name: string;
+  price: number;
+  image: string;
+}
+
+const App: React.FC = () => {
+  const [date, setDate] = useState(new Date());
+  const [counter, setCounter] = useState(0);
+  const [beers, setBeers] = useState<Beer[]>([]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://api.sampleapis.com/beers/ale")
+      .then((res) => res.json())
+      .then((data) => {
+        setBeers(data);
+      });
+  }, []);
+
+  // Décalage horaire
+  const shanghaiTime = new Date(date.getTime() + 8 * 60 * 60 * 1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Paris : {date.toLocaleDateString()}{" "}
+          {date.toLocaleTimeString("fr-FR")}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          Shanghai : {date.toLocaleDateString("zh-CN")}{" "}
+          {shanghaiTime.toLocaleTimeString("zh-CN")}
+        </p>
       </header>
+
+      <main>
+        <p>Compteur : {counter}</p>
+
+        <button onClick={() => setCounter(counter + 1)}>Incrémenter</button>
+        <button onClick={() => setCounter(counter - 1)}>Décrémenter</button>
+
+        <input
+          type="number"
+          value={counter}
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
+      </main>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prix</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+          {beers.map((beer) => (
+            <tr key={beer.name}>
+
+              <td>{beer.name}</td>
+
+              <td>{beer.price}</td>
+              <td>
+                <img src={beer.image} alt={beer.name} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
